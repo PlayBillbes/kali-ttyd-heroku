ead78b41071f5b477f8037199f81ffa74218caaa
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2023-07-29 02:58:14 +0630

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-relay login -k $KEY -s $SECRET
-echo "ttyd serving at port $PORT with username:pass as $USER:$PASSWORD"
+relay login -k ${KEY} -s ${SECRET}
+echo "ttyd serving at port $PORT with username:pass as ${USER}:${PASSWORD}"
 chmod +x /usr/local/bin/ttyd_linux
-/usr/local/bin/ttyd_linux -p $PORT -c $USER:$PASSWORD bash
+/usr/local/bin/ttyd_linux -p ${PORT} -c ${USER}:${PASSWORD} bash
